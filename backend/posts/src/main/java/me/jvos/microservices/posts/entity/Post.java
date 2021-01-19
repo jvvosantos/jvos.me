221e7e120c803b0d11628907d522319bfd58267e
jvvosantos <jvos@cin.ufpe.br>
2021-01-18 23:50:21 -0300

    creation of backend folder and posts service

---
+++ backend/posts/src/main/java/me/jvos/microservices/posts/entity/Post.java
@@ -0,0 +1,91 @@
+package me.jvos.microservices.posts.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Post {
+	
+	@Id
+	private String id;
+	
+	@CreatedDate
+	private Date createdDate;
+	
+	@LastModifiedDate
+	private Date lastModifiedDate;
+	
+	private String imageUrl;
+	
+	private String title;
+	
+	private String body;
+	
+	private List<String> tags;
+	
+	public Post() {
+
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public List<String> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<String> tags) {
+		this.tags = tags;
+	}
+
+}
